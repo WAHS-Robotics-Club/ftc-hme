432d9751624d3968256bcf970869c5c2423b51df
Nathaniel Glover <nathanielsglover@gmail.com>
2017-10-07 11:09:37 -0400

    Add HardwareMapConstants and basic tests

---
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/HardwareMapConstants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.team9202hme;
+
+
+public class HardwareMapConstants {
+    public static final String COLOR_SENSOR = "sensor-color";
+    public static final String GYRO_SENSOR = "sensor-gyro";
+
+    public static final String CLAW_LEFT_CRSERVO = "claw-left";
+    public static final String CLAW_RIGHT_CRSERVO = "claw-right";
+    public static final String CLAW_ARM_DCMOTOR = "claw-arm";
+
+    public static final String DRIVE_FRONT_LEFT = "front-left";
+    public static final String DRIVE_FRONT_RIGHT = "front-right";
+    public static final String DRIVE_BACK_LEFT = "back-left";
+    public static final String DRIVE_BACK_RIGHT = "back-right";
+}
---
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/tests/WheelTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.team9202hme.tests;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.team9202hme.HardwareMapConstants;
+
+@Autonomous(name = "Wheel Test", group = "Tests")
+public class WheelTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor frontLeft = hardwareMap.dcMotor.get(HardwareMapConstants.DRIVE_FRONT_LEFT);
+        DcMotor frontRight = hardwareMap.dcMotor.get(HardwareMapConstants.DRIVE_FRONT_RIGHT);
+        DcMotor backLeft = hardwareMap.dcMotor.get(HardwareMapConstants.DRIVE_BACK_LEFT);
+        DcMotor backRight = hardwareMap.dcMotor.get(HardwareMapConstants.DRIVE_BACK_RIGHT);
+
+        waitForStart();
+
+        frontLeft.setPower(1);
+        Thread.sleep(500);
+        frontLeft.setPower(0);
+
+        frontRight.setPower(1);
+        Thread.sleep(500);
+        frontRight.setPower(0);
+
+        backLeft.setPower(1);
+        Thread.sleep(500);
+        backLeft.setPower(0);
+
+        backRight.setPower(1);
+        Thread.sleep(500);
+        backRight.setPower(0);
+    }
+}
