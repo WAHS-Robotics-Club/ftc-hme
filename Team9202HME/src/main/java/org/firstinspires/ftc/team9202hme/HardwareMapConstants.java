0f3e9abc4d3d908e9a279cb2b46cc992e518383e
Nathaniel Glover <nathanielsglover@gmail.com>
2018-11-05 19:20:20 -0500

    Update Adafruit IMU calibration system

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/HardwareMapConstants.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/HardwareMapConstants.java
@@ -2,17 +2,8 @@ package org.firstinspires.ftc.team9202hme;
 
 
 public class HardwareMapConstants {
-    public static final String COLOR_DISTANCE_SENSOR = "colorRange";
+    public static final String CAMERA = "camera";
     public static final String IMU = "imu";
-
-    public static final String CLAW_LOWER_LEFT_SERVO = "low-left";
-    public static final String CLAW_LOWER_RIGHT_SERVO = "low-right";
-    public static final String CLAW_UPPER_LEFT_SERVO = "up-left";
-    public static final String CLAW_UPPER_RIGHT_SERVO = "up-right";
-    public static final String PULLEY_DCMOTOR = "pulley";
-
-    public static final String WHACKER_SERVO = "whacker";
-    public static final String WHACKER_EXTENDER_SERVO = "extender";
 
     public static final String DRIVE_FRONT_LEFT = "fl";
     public static final String DRIVE_FRONT_RIGHT = "fr";
