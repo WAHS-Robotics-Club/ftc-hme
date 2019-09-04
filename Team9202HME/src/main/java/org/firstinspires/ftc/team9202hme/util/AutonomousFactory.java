4c385e866ac1270795213c612fb54f55ce25aa81
cjak7 <cjak7@schls.albco>
2019-09-04 17:07:37 -0400

    @everyone sadly not

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/util/AutonomousFactory.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/util/AutonomousFactory.java
@@ -16,7 +16,6 @@ public class AutonomousFactory implements OpModeFactory {
     static {
         FtcOpModeRegister.registerOpModeFactory(new AutonomousFactory());
     }
-    //hi
     @Override
     public Map<OpMode, OpModeMeta> generateOpModes() {
         Map<OpMode, OpModeMeta> opModes = new HashMap<>();
