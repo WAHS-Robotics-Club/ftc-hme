5cac16a29da2c9af46e28b8d3922dc3037983a3f
ebwv6 <ebwv6@schls.albco>
2019-09-25 16:50:50 -0400

    servo class import

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.team9202hme;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name ="Test TeleOp")
 public class Test extends OpMode {
