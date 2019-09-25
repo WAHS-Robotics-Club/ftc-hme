db4aba75325994c7355787a44f4b5e35ae5b90cd
ebwv6 <ebwv6@schls.albco>
2019-09-25 16:38:24 -0400

    yodel

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
@@ -1,61 +1,29 @@
 package org.firstinspires.ftc.team9202hme;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-
 @TeleOp(name ="Test TeleOp")
 public class Test extends OpMode {
-DcMotor leftFront;
-DcMotor rightFront;
-DcMotor leftBack;
-DcMotor rightBack;
-Servo leftServo;
-Servo rightServo;
+    Servo leftServo;
+    Servo rightServo;
 
-        //hello EJ. I am inevitable. LOTR > Anime
+        @Override
+        public void init(){
+            leftServo = hardwareMap.servo.get("leftServo");
+            rightServo = hardwareMap.servo.get("rightServo");
+        }
 
-    @Override
-    public void init(){
-
-        leftFront = hardwareMap.get(DcMotor.class, "fl");
-        rightFront = hardwareMap.get(DcMotor.class, "fr");
-        leftBack = hardwareMap.get(DcMotor.class, "bl");
-        rightBack = hardwareMap.get(DcMotor.class, "br");
-
+        @Override public void loop(){
+            if(gamepad1.right_bumper) {
+                leftServo.setPosition(1);
+            }
+            else if(gamepad1.left_bumper){
+                leftServo.setPosition(1);
+            }
+            else{
+                leftServo.setPosition(0.5);
+                rightServo.setPosition(0.5);
+            }
     }
-/*
-leftFront = 2
-leftBack = 1
-rightFront = 3
-rightBack = 0
- */
-    @Override public void loop(){
-
-
-
-
-  double leftFront1 = (0);
-  double leftBack1 = (0);
-  double rightFront1 = (0);
-  double rightBack1 = (0);
-
-
-
-
-
-
-    leftBack.setPower(leftBack1);
-    leftFront.setPower(leftFront1);
-    rightBack.setPower(rightBack1);
-    rightFront.setPower(rightFront1);
-
-
-
-
-
-
-}
-
 }
