e1c720701eccb1fd2601f4fac99764fe7857d61a
Elijah <mutantprince633@gmail.com>
2019-09-25 17:16:44 -0400

    argh

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
@@ -14,7 +14,7 @@ public class Test extends OpMode {
             leftServo = hardwareMap.servo.get("leftServo");
             rightServo = hardwareMap.servo.get("rightServo");
         }
-    // hi
+    //
         @Override public void loop(){
             if(gamepad1.right_bumper) {
                 leftServo.setPosition(1);
