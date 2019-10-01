376002981b3e9af80331135f7347410e72320eca
Elijah <mutantprince633@gmail.com>
2019-10-01 16:42:47 -0400

    do you read these still gloveboy

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
@@ -14,7 +14,7 @@ public class Test extends OpMode {
             leftServo = hardwareMap.servo.get("leftServo");
             rightServo = hardwareMap.servo.get("rightServo");
         }
-    //lord of the rings is pretty good
+    //lord of the rings is pretty good and so is star wars
         @Override public void loop(){
             if(gamepad1.right_bumper) {
                 leftServo.setPosition(1);
