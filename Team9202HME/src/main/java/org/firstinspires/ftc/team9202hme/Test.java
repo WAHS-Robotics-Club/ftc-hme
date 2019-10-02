de88aecb4c43727ddb789223d1145d04551b69bc
ebwv6 <ebwv6@schls.albco>
2019-10-01 21:21:54 -0400

    Servo and drive code before major changes

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
@@ -4,18 +4,33 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name ="Servo test for Elijah")
+@TeleOp(name ="Elijah's drive and servo test")
 public class Test extends OpMode {
+    //Making the servos:
     Servo leftServo;
     Servo rightServo;
 
+    //Making the dc motors:
+    DcMotor flMotor;
+    DcMotor frMotor;
+    DcMotor blMotor;
+    DcMotor brMotor;
+
         @Override
         public void init(){
+            //Hardware mapping the servos:
             leftServo = hardwareMap.servo.get("leftServo");
             rightServo = hardwareMap.servo.get("rightServo");
-        }
-        @Override public void loop(){
 
+            //Hardware mapping the motors:
+            flMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+            frMotor = hardwareMap.dcMotor.get("frontRightMotor");
+            blMotor = hardwareMap.dcMotor.get("backLeftMotor");
+            brMotor = hardwareMap.dcMotor.get("backRightMotor");
+        }
+        @Override
+        public void loop(){
+            //If else statements for the right servo controls (bumpers):
             if(gamepad1.right_bumper) {
                 rightServo.setPosition(1);
             }
@@ -23,13 +38,12 @@ public class Test extends OpMode {
                 rightServo.setPosition(0.5);
             }
 
+            //If else statements for the left servo controls (bumpers);
             if(gamepad1.left_bumper){
                 leftServo.setPosition(-0);
             }
             else{
                 leftServo.setPosition(0.5);
             }
-
-
     }
 }
