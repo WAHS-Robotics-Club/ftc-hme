c34c60238dea5f25b340db980c8e43f58e3c22ae
ebwv6 <mutantprince633@gmail.com>
2019-10-04 21:18:38 -0400

    Added drive code to servo and spool test code

--- Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
+++ Team9202HME/src/main/java/org/firstinspires/ftc/team9202hme/Test.java
@@ -6,43 +6,45 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name ="E J's drive and servo test")
 public class Test extends OpMode {
+    //Initializing the dc motor objects:
     DcMotor frontLeft;
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
-    /*
+
     //Initializing the servo objects:
     Servo leftServo;
     Servo rightServo;
 
     //Initializing the dc motor objects:
     DcMotor spoolMotor;
-    */
+
         @Override
         public void init(){
+            //Hardware mapping the drive motors:
             frontLeft = hardwareMap.dcMotor.get("frontLeftMotor");
             frontRight = hardwareMap.dcMotor.get("frontRightMotor");
             backLeft = hardwareMap.dcMotor.get("backLeftMotor");
             backRight = hardwareMap.dcMotor.get("backRightMotor");
-            /*
+
             //Hardware mapping the servos:
             leftServo = hardwareMap.servo.get("leftServo");
             rightServo = hardwareMap.servo.get("rightServo");
 
-            //Hardware mapping the motors:
+            //Hardware mapping the miscellaneous motors:
             spoolMotor = hardwareMap.dcMotor.get("spoolMotor");
-            */
-
         }
         @Override
         public void loop(){
+            //Drive code (currently tank mode with both sticks; changing to actually good alternative soon):
             frontLeft.setPower(gamepad1.left_stick_y);
             frontRight.setPower(gamepad1.right_stick_y);
-
             backLeft.setPower(gamepad1.left_stick_y);
             backRight.setPower(gamepad1.right_stick_y);
-            /*
-            //If else statements for the right servo controls (bumpers):
+
+            //Arm servo controls (right and left bumpers):
+
+            //Right servo:
             if(gamepad1.right_bumper) {
                 rightServo.setPosition(0.75);
             }
@@ -50,13 +52,15 @@ public class Test extends OpMode {
                 rightServo.setPosition(0);
             }
 
-            //If else statements for the left servo controls (bumpers);
+            //Left servo
             if(gamepad1.left_bumper){
                 leftServo.setPosition(0.25);
             }
             else{
                 leftServo.setPosition(1);
             }
+
+            //Spool motor code (left and right triggers):
 
             //Cancels out left and right triggers if pushed at the same time:
             if(gamepad1.left_trigger >= .1 && gamepad1.right_trigger >= .1){
@@ -72,6 +76,5 @@ public class Test extends OpMode {
             if(gamepad1.right_trigger >= .1){
                 spoolMotor.setPower(-gamepad1.left_trigger);
             }
-            */
     }
 }
