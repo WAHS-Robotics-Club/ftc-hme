644793c532d24a07186c1718ffc9075257c4641a
Charlie <calhusen@gmail.com>
2019-10-08 16:30:35 -0400

    created the grabber class and set up an initilization function

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/Grabber.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.team9202hme;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Grabber {
+    Servo leftServo;
+    Servo rightServo;
+
+    DcMotor spoolMotor;
+
+    public static Grabber initGrabber(HardwareMap hardwareMap){
+        //Creates and hardware maps the grabber element
+        Grabber grabber = new Grabber();
+
+        grabber.leftServo = hardwareMap.servo.get("leftServo");
+        grabber.rightServo = hardwareMap.servo.get("rightServo");
+
+        grabber.spoolMotor = hardwareMap.dcMotor.get("spoolMotor");
+
+
+        return grabber;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/MainTeleOp.java
@@ -9,21 +9,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name ="Main TeleOp - Charlie")
 public class MainTeleOp extends OpMode {
     //Initializing the servo objects:
-    Servo leftServo;
-    Servo rightServo;
 
-    DcMotor spoolMotor;
-
+    Grabber grabber;
     DriveTrain driveTrain;
 
 
     @Override
     public void init(){
         //Hardware mapping the servos:
-        leftServo = hardwareMap.servo.get("leftServo");
-        rightServo = hardwareMap.servo.get("rightServo");
-
-        spoolMotor = hardwareMap.dcMotor.get("spoolMotor");
+        Grabber grabber = Grabber.initGrabber(hardwareMap);
         DriveTrain driveTrain = DriveTrain.initDriveTrain(hardwareMap);
 
     }
@@ -36,18 +30,18 @@ public class MainTeleOp extends OpMode {
     @Override public void loop(){
         //If else statements for the right servo controls (bumpers):
         if(gamepad1.right_bumper) {
-            rightServo.setPosition(0.25);
+            grabber.rightServo.setPosition(0.25);
         }
         else{
-            rightServo.setPosition(1);
+            grabber.rightServo.setPosition(1);
         }
 
         //If else statements for the left servo controls (bumpers);
         if(gamepad1.left_bumper){
-            leftServo.setPosition(0.75);
+            grabber.leftServo.setPosition(0.75);
         }
         else{
-            leftServo.setPosition(0);
+            grabber.leftServo.setPosition(0);
         }
 
         //Drive Train controls w/ math for diagonal controls
@@ -58,13 +52,13 @@ public class MainTeleOp extends OpMode {
         //Left Trigger and Right trigger move opposite directions
         //Both triggers cancel each other out
         if(gamepad1.left_trigger >= .1 && gamepad1.right_trigger >= .1){
-            spoolMotor.setPower(0);
+            grabber.spoolMotor.setPower(0);
         }else if(gamepad1.left_trigger >= .1){
-            spoolMotor.setPower(gamepad1.right_trigger);
+            grabber.spoolMotor.setPower(gamepad1.right_trigger);
         }else if(gamepad1.right_trigger >= .1){
-            spoolMotor.setPower(-gamepad1.left_trigger);
+            grabber.spoolMotor.setPower(-gamepad1.left_trigger);
         }else{
-            spoolMotor.setPower(0);
+            grabber.spoolMotor.setPower(0);
         }
 
 
