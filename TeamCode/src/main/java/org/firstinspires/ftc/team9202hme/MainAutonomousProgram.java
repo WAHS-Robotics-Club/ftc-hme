4482ebc6a8e2bd0af51a27d78a463d393d5e2a48
Charlie <calhusen@gmail.com>
2019-10-21 16:07:38 -0400

    AUTONOMOUSITIZATIONING

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/MainAutonomousProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/MainAutonomousProgram.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.team9202hme;
 
-public class MainAutonomousProgram extends AutonomousProgram {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Autonomous(name ="Main Autonomous - Charlie")
+public class MainAutonomousProgram extends LinearOpMode {
+    DriveTrain driveTrain;
+    Grabber grabber;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveTrain = DriveTrain.initDriveTrain(hardwareMap);
+        grabber = Grabber.initGrabber(hardwareMap);
+
+
+        
+    }
 }
