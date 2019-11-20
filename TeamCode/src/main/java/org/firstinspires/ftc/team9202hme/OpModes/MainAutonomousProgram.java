fa3730496a4cfbcda8e66fad2179fe950a3a698c
Charlie <calhusen@gmail.com>
2019-11-20 16:11:44 -0500

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/OpModes/MainAutonomousProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/OpModes/MainAutonomousProgram.java
@@ -6,17 +6,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.team9202hme.Objects.BananaFruit;
 import org.firstinspires.ftc.team9202hme.Objects.DriveTrain;
 import org.firstinspires.ftc.team9202hme.Objects.Grabber;
+import org.firstinspires.ftc.team9202hme.Objects.Misc;
 
 @Autonomous(name ="Main Autonomous - Charlie")
 public class MainAutonomousProgram extends LinearOpMode {
 
     DriveTrain driveTrain;
     Grabber grabber;
+    Misc misc;
 
     @Override
     public void runOpMode() throws InterruptedException {
         driveTrain = DriveTrain.initDriveTrain(hardwareMap);
         grabber = Grabber.initGrabber(hardwareMap);
+        misc = Misc.initMiscellaneous(hardwareMap);
         int i;
 
         telemetry.addData("IsBusy", driveTrain.isBusy());
