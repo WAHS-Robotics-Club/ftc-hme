af4d9af3940500d5b3e4f5f0db1390dfa94c7ce2
Charlie <calhusen@gmail.com>
2020-01-14 17:01:44 -0500

    added parking

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/OpModes/Autonomi/RedSide/RedFoundationSideParking.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.team9202hme.OpModes.Autonomi.RedSide;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.team9202hme.Objects.BananaFruit;
+import org.firstinspires.ftc.team9202hme.Objects.DriveTrain;
+import org.firstinspires.ftc.team9202hme.Objects.Grabber;
+import org.firstinspires.ftc.team9202hme.Objects.Misc;
+
+@Autonomous(name ="RedFoundationSide - Parking", group = "Red Side")
+public class RedFoundationSideParking extends LinearOpMode {
+
+    DriveTrain driveTrain;
+    Grabber grabber;
+    Misc misc;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveTrain = DriveTrain.initDriveTrain(hardwareMap);
+        grabber = Grabber.initGrabber(hardwareMap);
+        misc = Misc.initMiscellaneous(hardwareMap);
+
+        telemetry.addData("IsBusy", driveTrain.isBusy());
+        driveTrain.logTelemetry(telemetry, driveTrain);
+        telemetry.update();
+        driveTrain.resetEncoders();
+        BananaFruit gyro = new BananaFruit();
+        gyro.runBananaFruit(hardwareMap, telemetry);
+        telemetry.update();
+
+        waitForStart();
+
+        driveTrain.moveForwardsBy(telemetry, 2);
+        driveTrain.turnToHeading(gyro, telemetry, 90);
+        driveTrain.moveForwardsBy(telemetry, 20);
+        //ONLY MODIFY STUFF AFTER THIS
+
+    }
+}
