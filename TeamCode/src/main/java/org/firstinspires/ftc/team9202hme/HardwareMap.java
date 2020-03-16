b1ce7059f204fe9ab52075a32bac19961feb7eec
ebwv6 <mutantprince633@gmail.com>
2020-03-16 07:58:21 -0400

    git is anooying

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/HardwareMap.java
@@ -1,4 +1,11 @@
 package org.firstinspires.ftc.team9202hme;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class HardwareMap {
+    public HardwareMap(DcMotor fl, DcMotor bl, DcMotor fr, DcMotor br){
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/SingleDriverTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/SingleDriverTeleOp.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.team9202hme;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class SingleDriverTeleOp {
+@TeleOp(name = "HME - SingleDriverTeleOp")
+public class SingleDriverTeleOp extends OpMode{
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+
+    @Override
+    public void init(){
+        
+    }
+
+    @Override
+    public void loop(){
+
+    }
 }
