4d0b3887bd66a70261879868e261453cba73f8b9
Elijah <mutantprince633@gmail.com>
2021-10-01 17:39:25 -0400

    simple drive train

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TeachingCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TeachingCode.java
@@ -4,14 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "ElijahCode - TeachingTeleOpOne")
+@TeleOp(name = "ElijahCode - Simple Drive")
 public class SingleDriverTeleOp extends OpMode{
     DcMotor fl;
     DcMotor fr;
     DcMotor bl;
     DcMotor br;
-
-    Servo s;
 
     @Override
     public void init(){
@@ -19,8 +17,6 @@ public class SingleDriverTeleOp extends OpMode{
         fr = hardwareMap.dcMotor.get("frontRightMotor");
         bl = hardwareMap.dcMotor.get("backLeftMotor");
         br = hardwareMap.dcMotor.get("backRightMotor");
-
-        s = hardwareMap.servo.get("servo");
     }
 
     @Override
@@ -29,12 +25,5 @@ public class SingleDriverTeleOp extends OpMode{
         fr.setPower(gamepad1.left_stick_x + gamepad1.left_stick_y + -gamepad1.right_stick_x);
         bl.setPower(-gamepad1.left_stick_x + -gamepad1.left_stick_y + -gamepad1.right_stick_x);
         br.setPower(gamepad1.left_stick_x + -gamepad1.left_stick_y + -gamepad1.right_stick_x);
-
-        if(gamepad1.right_bumper == 1) {
-            s.setPosition(0);
-        }
-        else if(gampad1.left_bumper == 1){
-            s.setPosition(1);
-        }
     }
 }
