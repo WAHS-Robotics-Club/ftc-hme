c44f194efbcef772ca7762597de55fecdf0d90f8
Charlie <calhusen@gmail.com>
2021-10-06 17:46:09 -0400

    yeet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/DriveTrain.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.team9202hme;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class DriveTrain {
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+
+    public DriveTrain(DcMotor fl, DcMotor bl, DcMotor fr, DcMotor br){
+        frontLeftMotor = fl;
+        backLeftMotor = bl;
+        frontRightMotor = fr;
+        backRightMotor = br;
+    }
+
+    public void regDrive(){
+        frontLeftMotor.setPower(-gamepad1.left_stick_x + gamepad1.left_stick_y + -gamepad1.right_stick_x);
+        backLeftMotor.setPower(gamepad1.left_stick_x + gamepad1.left_stick_y + -gamepad1.right_stick_x);
+        frontRightMotor.setPower(-gamepad1.left_stick_x + -gamepad1.left_stick_y + -gamepad1.right_stick_x);
+        backRightMotor.setPower(gamepad1.left_stick_x + -gamepad1.left_stick_y + -gamepad1.right_stick_x);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TestEnvironment.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.team9202hme;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name ="Test Environment (Auto or TeleOp)")
+public class Test extends OpMode {
+    DcMotor frontLeftMotor;
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+
+    HardwareMap hardware = new HardwareMap(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
+    DriveTrain drive = new DriveTrain(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
+
+    @Override
+    public void init(){
+        hardware.motorHardware();
+        drive.regDrive();
+    }
+
+    @Override
+    public void loop() {
+        drive.regDrive();
+    }
+}
