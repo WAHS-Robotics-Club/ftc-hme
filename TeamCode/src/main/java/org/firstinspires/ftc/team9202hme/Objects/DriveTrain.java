17e7c6d6aead96696c28757b7f30c6bbaa02cf47
Charlie <calhusen@gmail.com>
2021-10-06 16:07:29 -0400

    gradle evil

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/Objects/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/Objects/DriveTrain.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.team9202hme.Tool.Toggle;
 
 public class DriveTrain{
     //Initializing the dc motor objects:
+    //Gradle Evil
 
     DcMotor flMotor;
     DcMotor frMotor;
