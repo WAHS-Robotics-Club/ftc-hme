a4a8a3ecd730495584ec6300e495de5e32aa996f
Elijah <mutantprince633@gmail.com>
2021-10-07 12:13:52 -0400

    fixing my end maybe

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/Testing.java
@@ -2,10 +2,9 @@ package org.firstinspires.ftc.team9202hme;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name ="Test Environment (Auto or TeleOp)")
-public class Test extends OpMode {
+public class Testing extends OpMode {
     DcMotor frontLeftMotor;
     DcMotor backLeftMotor;
     DcMotor frontRightMotor;
