82272bb5d3769b5660035079e7aed4bb4d78e3ef
Charlie <calhusen@gmail.com>
2021-10-18 17:43:51 -0400

    set up master branch for tree time

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/SingleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/SingleDriver.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+//You are on the master branch
+
 @TeleOp(name ="Single Driver TeleOp")
 public class SingleDriver extends OpMode {
     //Local DcMotor variables:
