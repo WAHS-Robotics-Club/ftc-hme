60c71a58872b7c14b2011da41ddb29d6c497746c
Elijah <mutantprince633@gmail.com>
2021-12-03 16:58:57 -0500

    gamer gamer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TestAutonomousTwo.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.team9202hme;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/*
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+*/
+
+@Autonomous(name ="Test Auto 2")
+public class TestAutonomousTwo extends LinearOpMode {
+
+    //Local DcMotor variables:
+    DcMotor fl;
+    DcMotor bl;
+    DcMotor fr;
+    DcMotor br;
+    DcMotor spool;
+
+    //Local CRServo and Servo variables:
+    CRServoImplEx carousel;
+
+    DriveTrain driveTrain;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveTrain = DriveTrain.initDriveTrain(hardwareMap);
+
+        telemetry.addData("IsBusy", driveTrain.isBusy());
+        driveTrain.logTelemetry(telemetry, driveTrain);
+        telemetry.update();
+        driveTrain.resetEncoders();
+        BananaFruit gyro = new BananaFruit();
+        gyro.runBananaFruit(hardwareMap, telemetry);
+        telemetry.update();
+
+        waitForStart();
+
+        //ONLY MODIFY STUFF AFTER THIS
+        sleep(1000);
+        driveTrain.moveForwardsBy(telemetry, 20);
+        driveTrain.turnToHeading(gyro, telemetry,180);
+    }
+}
