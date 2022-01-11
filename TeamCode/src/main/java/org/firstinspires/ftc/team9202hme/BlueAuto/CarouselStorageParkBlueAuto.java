33946cc1262a4f6034829e15b6f244ef4a9cd2b8
Elijah <mutantprince633@gmail.com>
2022-01-11 11:55:40 -0500

    charlie do pull thing

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/BlueAuto/CarouselStorageParkBlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/BlueAuto/CarouselStorageParkBlueAuto.java
@@ -15,7 +15,7 @@ YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUC
 @Autonomous(name ="Carousel Storage Park BLU RIGHT")
 public class CarouselStorageParkBlueAuto extends LinearOpMode {
 
-    //Local DcMotor variables: 
+    //Local DcMotor variables:
     DcMotor fl;
     DcMotor bl;
     DcMotor fr;
