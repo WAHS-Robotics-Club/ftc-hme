2a46c1c81b6c3b249d8927546369fc42fa955e4f
Elijah <mutantprince633@gmail.com>
2022-01-21 14:54:29 -0500

    more auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto/ScoreRedAuto.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.RedAuto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.PwmControl;
+
+import org.firstinspires.ftc.teamcode.BananaFruit;
+import org.firstinspires.ftc.teamcode.DriveTrain;
+
+/*
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+*/
+
+@Autonomous(name ="Warehouse Score RED")
+public class WarehouseParkScoreRedAuto extends LinearOpMode {
+
+    //Local DcMotor variables:
+    DcMotor fl;
+    DcMotor bl;
+    DcMotor fr;
+    DcMotor br;
+    DcMotor spool;
+    DcMotor grab;
+
+    //Local CRServo and Servo variables:
+    CRServoImplEx carousel;
+
+    DriveTrain driveTrain;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        driveTrain = DriveTrain.initDriveTrain(hardwareMap);
+        grab = hardwareMap.dcMotor.get("grab");
+        spool = hardwareMap.dcMotor.get("spoolMotor");
+        carousel = (CRServoImplEx) hardwareMap.crservo.get("carouselSpinner");
+        carousel.setPwmRange(new PwmControl.PwmRange(553,2520));
+
+        telemetry.addData("IsBusy", driveTrain.isBusy());
+        driveTrain.logTelemetry(telemetry, driveTrain);
+        telemetry.update();
+        driveTrain.resetEncoders();
+        BananaFruit gyro = new BananaFruit();
+        gyro.runBananaFruit(hardwareMap, telemetry);
+        telemetry.update();
+
+        waitForStart();
+
+        //ONLY MODIFY STUFF AFTER THIS
+
+        //waits .25 seconds, goes forward 24 in, turns carousel for 5 seconds:
+        sleep(250);
+        driveTrain.moveForwardsBy(telemetry, -34);
+
+        driveTrain.turnToHeading(gyro, telemetry,90);
+        driveTrain.moveForwardsBy(telemetry, -4);
+        grab.setPower(.25);
+        sleep(500);
+        grab.setPower(-.25);
+        sleep(500);
+
+        driveTrain.moveForwardsBy(telemetry, 4);
+        driveTrain.turnToHeading(gyro, telemetry, 0);
+        driveTrain.moveForwardsBy(telemetry, 32);
+        driveTrain.turnToHeading(gyro, telemetry, -90);
+        driveTrain.moveForwardsBy(telemetry, 30);
+    }
+}
