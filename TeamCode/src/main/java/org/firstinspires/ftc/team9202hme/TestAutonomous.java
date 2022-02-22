6feff5d5459ed8ad30a932ab2a75c8c1da479358
Elijah <mutantprince633@gmail.com>
2022-02-22 12:06:27 -0500

    setting up more stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TestAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TestAutonomous.java
@@ -1,18 +1,47 @@
 package org.firstinspires.ftc.team9202hme;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name ="Single Driver")
-public class TestAutonomous extends OpMode {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.PwmControl;
 
+/*
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+YOU ARE ON THE MASTER BRANCH (!) (!) (!) (!) (!) DO NOT CODE HERE IF NOT INSTRUCTED (!) (!) (!)
+*/
+
+@Autonomous(name ="Warehouse RED")
+public class TestAutonomous extends LinearOpMode {
+
+    //Local DcMotor variables:
+    DcMotor fl;
+    DcMotor bl;
+    DcMotor fr;
+    DcMotor br;
+    DcMotor spool;
+    DcMotor grab;
+
+    //Local CRServo and Servo variables:
+    DcMotor carousel;
 
     @Override
-    public void init(){
+    public void runOpMode() throws InterruptedException {
+        fl = hardwareMap.dcMotor.get("frontLeftMotor");
+        bl = hardwareMap.dcMotor.get("backLeftMotor");
+        fr = hardwareMap.dcMotor.get("frontRightMotor");
+        br = hardwareMap.dcMotor.get("backRightMotor");
 
-    }
+        spool = hardwareMap.dcMotor.get("spoolSpinner");
+        grab = hardwareMap.dcMotor.get("grab");
+        carousel = hardwareMap.dcMotor.get("carouselSpinner");
 
-    @Override
-    public void loop() {
+
+        waitForStart();
+        sleep(250);
+
+        //ONLY MODIFY STUFF AFTER THIS
 
     }
 }
