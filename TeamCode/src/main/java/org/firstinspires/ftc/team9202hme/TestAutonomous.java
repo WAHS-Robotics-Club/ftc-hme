bb9664705ccbf58e91e1d8e05d6a43eef81a1d76
Elijah <mutantprince633@gmail.com>
2022-02-22 11:59:56 -0500

    setting up testing environments

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team9202hme/TestAutonomous.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.team9202hme;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name ="Single Driver")
+public class TestAutonomous extends OpMode {
+
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
